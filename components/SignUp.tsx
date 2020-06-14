dab619a65357ad010ce8624dd82c362407b19d90
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2020-06-14 18:17:50 +0100

    with Zod

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Checkbox from '@material-ui/core/Checkbox'
 import Link from '@material-ui/core/Link'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
@@ -12,13 +10,15 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Yum Meals
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -46,9 +46,51 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+const validationSchema = z
+  .object({
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    email: z.string().email(),
+    password: z.string(),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword)
+
+type Inputs = z.infer<typeof validationSchema>
+
+const getValidationResolver = (validationSchema: any) => (data: unknown) => {
+  try {
+    const values = validationSchema.parse(data)
+
+    return {
+      values,
+      errors: {},
+    }
+  } catch (error) {
+    console.dir(error.errors)
+    return {
+      values: {},
+      errors: error.errors.reduce(
+        (allErrors: any, currentError: any) => ({
+          ...allErrors,
+          [currentError.path]: {
+            type: currentError.type ?? 'validation',
+            message: currentError.message,
+          },
+        }),
+        {}
+      ),
+    }
+  }
+}
 export default function SignUp() {
   const classes = useStyles()
 
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>({
+    validationResolver: getValidationResolver(validationSchema),
+  })
+  const onSubmit = (data: Inputs) => console.log(data)
+  console.log(watch('firstName'))
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -59,29 +101,33 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
                 autoComplete="fname"
                 name="firstName"
                 variant="outlined"
-                required
                 fullWidth
                 id="firstName"
                 label="First Name"
                 autoFocus
+                inputRef={register}
+                error={Boolean(errors?.firstName)}
+                helperText={errors?.firstName?.message}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 variant="outlined"
-                required
                 fullWidth
                 id="lastName"
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
+                inputRef={register}
+                error={Boolean(errors?.lastName)}
+                helperText={errors?.lastName?.message}
               />
             </Grid>
             <Grid item xs={12}>
@@ -93,6 +139,9 @@ export default function SignUp() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                inputRef={register({ required: true })}
+                error={Boolean(errors?.email)}
+                helperText={errors?.email?.message}
               />
             </Grid>
             <Grid item xs={12}>
@@ -105,12 +154,24 @@ export default function SignUp() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                inputRef={register({ required: true })}
+                error={Boolean(errors?.password)}
+                helperText={errors?.password?.message}
               />
             </Grid>
             <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="confirmPassword"
+                label="Confirm your Password"
+                type="password"
+                id="confirmPassword"
+                autoComplete="current-password"
+                inputRef={register({ required: true })}
+                error={Boolean(errors?.confirmPassword)}
+                helperText={errors?.confirmPassword?.message}
               />
             </Grid>
           </Grid>
